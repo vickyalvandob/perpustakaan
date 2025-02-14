a72858ff9156b8f27d34507ebca5d47c22e73b77
Vicky Alvando <73622790+vickyalvandob@users.noreply.github.com>
2025-02-14 15:03:15 +0700

    add utils

--- resources/js/Components/InputError.jsx
+++ resources/js/Components/InputError.jsx
@@ -1,9 +1,8 @@
+import { cn } from '@/lib/utils';
+
 export default function InputError({ message, className = '', ...props }) {
     return message ? (
-        <p
-            {...props}
-            className={'text-sm text-red-600 ' + className}
-        >
+        <p {...props} className={cn('text-xs font-medium text-red-600', className)}>
             {message}
         </p>
     ) : null;
--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,6 +1,60 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+export const FINEPAYMENTSTATUS = {
+    PENDING: 'TERTUNDA',
+    SUCCESS: 'Sukses',
+    FAILED: 'Gagal',
+};
+
+export function flashMessage(params) {
+    return params.props.flash_message;
+}
+
+export const formatToRupiah = (amount) => {
+    const formatter = new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    });
+
+    return formatter.format(amount);
+};
+
+export const messages = {
+    503: {
+        title: 'Service Unavailable',
+        description: 'Sorry, we are doing some maintenance. Please check back soon',
+        status: '503',
+    },
+    500: {
+        title: 'Server Error',
+        description: 'Oops, something went wrong on our servers',
+        status: '500',
+    },
+    404: {
+        title: 'Not Found',
+        description: 'Sorry, the page you are looking for could not be found',
+        status: '404',
+    },
+    403: {
+        title: 'Forbidden',
+        description: 'Sorry, you are forbidden from accessing this page',
+        status: '403',
+    },
+    401: {
+        title: 'Unauthorized',
+        description: 'Sorry, you are unauthorized to access this page',
+        status: '104',
+    },
+    429: {
+        title: 'Too Many Request',
+        description: 'Please try again in just a second',
+        status: '429',
+    },
+};
